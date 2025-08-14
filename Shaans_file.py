1fb743da8a291f775d87d2d3839bacc358b96dee
shaanskooner-cell <shaanskooner@gmail.com>
2025-08-14 16:43:40 -0700

    Create Shaans_file.py

---
+++ Shaans_file.py
@@ -0,0 +1,11 @@
+print("The game has started")
+input("Press enter to continue")
+print("A portal opens up in your room and a wizard appears")
+input("Press enter to continue")
+print("He gives you two options")
+print("A red door or a blue door")
+input("Which door do you choose?)
+if player_choice == red 
+      print("You have now entered a dark forest with complete silence!')
+if player_choice == blue 
+      print("You have now entered an island surrounded by water!")
